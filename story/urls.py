e5216389f127fa6ead72bfbd047398f6a8d36d62
Marwan Sabih <marwansabih@gmx.de>
2023-01-09 13:19:33 +0100

    first time dialogs testable

--- story/urls.py
+++ story/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("story/<int:id>", views.story, name="story"),
     path("actor", views.actor, name="actor"),
+    path("dialog", views.dialog, name="dialog")
 ]
--- story/views.py
+++ story/views.py
@@ -1,9 +1,26 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, reverse, redirect
+from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
+from django.views.decorators.csrf import csrf_exempt
+import json
 
 def story(request, id):
+    print("HI")
     return render(request, "story/show.html")
 
 
 def actor(request):
+    if request == "POST":
+        return HttpResponse("HEY")
     return render(request, "story/actor.html")
+
+
+@csrf_exempt
+def dialog(request):
+    print(request)
+    if request == "POST":
+        data = json.loads(request.body)
+        print(data)
+    return render(request, "story/dialog.html")
+    #render(request, "story/success.html")
+    #redirect("show", id=1)
+    #HttpResponseRedirect(reverse("story", kwargs={"id": 1})) #JsonResponse({"message": "Character successfully created."}, status=201)
