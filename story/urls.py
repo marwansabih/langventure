6ddf67397fce6a9bf8049984bc827292671d677d
Marwan Sabih <marwansabih@gmx.de>
2023-01-15 15:07:08 +0100

    updated story

--- story/urls.py
+++ story/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path("story/<int:id>", views.story, name="story"),
+    path("create_story/", views.create_story, name="create_story"),
     path("actor", views.actor, name="actor"),
     path("dialog", views.dialog, name="dialog")
 ]
--- story/views.py
+++ story/views.py
@@ -3,10 +3,14 @@ from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
 from django.views.decorators.csrf import csrf_exempt
 import json
 
+
 def story(request, id):
     print("HI")
     return render(request, "story/show.html")
 
+
+def create_story(request):
+    return render(request, "story/create_story.html")
 
 def actor(request):
     if request == "POST":
