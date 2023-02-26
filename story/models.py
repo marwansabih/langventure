074c55a96300e651a82bc2f9f4e1ff241c1f1f4f
Marwan Sabih <marwansabih@gmx.de>
2023-02-26 20:15:14 +0100

    fixed wrong element chaos with javascript uuid

--- story/models.py
+++ story/models.py
@@ -31,26 +31,17 @@ class Dialog(models.Model):
     translation = models.TextField(default="")
 
 
+class Knowledge(models.Model):
+    item = models.CharField(max_length=50)
+    story = models.ForeignKey(Story, on_delete=models.CASCADE, blank=True, null=True, related_name="knowledge_items")
+    users = models.ManyToManyField(User, related_name="knowledge_items")
+
+
 class Option(models.Model):
     origin = models.ForeignKey(Dialog, on_delete=models.CASCADE, related_name="options")
     target = models.ForeignKey(Dialog, on_delete=models.CASCADE, blank=True, null=True)
     text = models.TextField()
     translation = models.TextField(default="")
-
-
-class UserStoryConfig(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="stories")
-    story = models.ForeignKey(Story, on_delete=models.CASCADE, related_name="configs")
-    current_dialog = models.ForeignKey(Dialog, on_delete=models.CASCADE)
-
-
-class Knowledge(models.Model):
-    item = models.CharField(max_length=50)
-
-
-class OptionTokens(models.Model):
-    option = models.ForeignKey(Option, on_delete=models.CASCADE, related_name="tokens")
-    word = models.CharField(max_length=50)
     acquired = models.ForeignKey(
         Knowledge,
         on_delete=models.CASCADE,
@@ -60,6 +51,17 @@ class OptionTokens(models.Model):
     )
     required_k_items = models.ManyToManyField(Knowledge, related_name="require_options")
     disabled_k_items = models.ManyToManyField(Knowledge, related_name="disabled_options")
+
+
+class UserStoryConfig(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="stories")
+    story = models.ForeignKey(Story, on_delete=models.CASCADE, related_name="configs")
+    current_dialog = models.ForeignKey(Dialog, on_delete=models.CASCADE)
+
+
+class OptionTokens(models.Model):
+    option = models.ForeignKey(Option, on_delete=models.CASCADE, related_name="tokens")
+    word = models.CharField(max_length=50)
 
 
 class DialogTokens(models.Model):
